9be53b2e7a06b09e58b712577ef5a560b34b89d8
Ultrin <joegimblett@gmail.com>
2024-10-12 09:51:54 -0400

    JG - added gitignore, main.sh, src dir, main and textnode pythong files in src dir

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from textnode import TextNode
+
+def main():
+    test = TextNode('This is a text node', text_type_bold, 'https://www.boot.dev')
+
+    print(test)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
